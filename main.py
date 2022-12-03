da10d4217cc1cd13ede8a8eb1dbf562de152e40a
salgue441 <carlossalguero441@gmail.com>
2022-12-03 12:57:20 -0600

    Finished Code

--- main.py
+++ main.py
@@ -20,4 +20,24 @@ labels.head()
 
 # Split the dataset into training and testing sets
 x_train, x_test, y_train, y_test = train_test_split(
-    df['text'], labels, test_size=2.0, random_state=7)
+    df['text'], labels, test_size=0.2, random_state=7)
+
+# Initializing a TFidfVectorizer
+tfidf_vectorizer = TfidfVectorizer(stop_words="english", max_df=0.7)
+
+# Fit and transform train set, transform test set
+tfidf_train = tfidf_vectorizer.fit_transform(x_train)
+tfidf_test = tfidf_vectorizer.transform(x_test)
+
+# Initialize a PassiveAggresiveClassifier
+pac = PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train, y_train)
+
+# Predict on the test set and calculates accuracy
+y_pred = pac.predict(tfidf_test)
+score = accuracy_score(y_test, y_pred)
+
+print(f"Accuracy: {round((score * 100), 2)}%")
+
+# Building a Confusion Matrix
+confusion_matrix(y_test, y_pred, labels=['FAKE', 'REAL'])
